2b66281b65bcb01712300567cdc1cfd13aaa4bd5
tigran024 <106924045+tigran024@users.noreply.github.com>
2025-01-12 19:48:23 +0400

    Update database.py

--- database.py
+++ database.py
@@ -1,10 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Database connection URL
 DATABASE_URL = "postgresql://TAexam:123@83.149.198.142:5406/TAexamdb"
 
-# SQLAlchemy setup
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
